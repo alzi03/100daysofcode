d2339a9f36ac2b923ee4d72ee6a0d6c059f99425
alzi03 <102617262+alzi03@users.noreply.github.com>
2022-10-08 22:25:44 -0400

    Update and rename Day7-hangman.py to 07-hangman.py

--- 07-hangman.py
+++ 07-hangman.py
@@ -57,9 +57,9 @@ word_list = ["aardvark", "baboon", "animal", "monkey", "train", "car" ,"zoo", "a
 def hangman(): 
   choice=random.choice(word_list)
   display = '_' * len(choice)
-  wrong =  0
-  guesses=[]
-  while wrong < 7:
+  wrong =  0 
+  guesses=[] #puts the guesses so player can't be penalized for repeats
+  while wrong < 7: #timer for num of gueses
     guess=input('Guess a letter: ').lower()
     index = 0
     indexes=[]
@@ -70,9 +70,9 @@ def hangman():
           indexes.append(index)
         index+=1
       for value in indexes:
-        display = display[:value] + guess + display[value+1:]
+        display = display[:value] + guess + display[value+1:] #rewriting output to include correct guesses
     else:
-      print(hangmanpics[wrong])
+      print(hangmanpics[wrong]) #poor hangman
       guesses.append(guess)
       wrong += 1
     print('\n')
@@ -80,7 +80,7 @@ def hangman():
     if '_' in display:
       continue
     else:
-      print('Good Job!')
+      print('Good Job!') #winning
       break
   if wrong == 7 : 
     print (f'Better luck next time, your word was {choice}')
